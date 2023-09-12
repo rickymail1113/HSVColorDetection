6816d84efbb3572a144cca7914f4e4911483a5b4
rickymail1113 <rickymail1113@gmail.com>
2023-09-12 20:41:16 +0800

    上傳opencv顏色偵測與過濾

---
+++ ColorDetectionCamera.py
@@ -0,0 +1,46 @@
+import cv2
+import numpy as np
+
+def empty(v):
+    return v
+
+
+
+#控制視窗
+cv2.namedWindow("TrackBar")
+cv2.resizeWindow("TrackBar", 640, 320)
+cv2.createTrackbar("Hue min", "TrackBar", 0, 179, empty) # 0~179 default 0
+cv2.createTrackbar("Hue max", "TrackBar", 255, 255, empty) # 0~255 default 255
+cv2.createTrackbar("Sat min", "TrackBar", 0, 179, empty) # 0~179 default 0
+cv2.createTrackbar("Sat max", "TrackBar", 255, 255, empty) # 0~255 default 255
+cv2.createTrackbar("Val min", "TrackBar", 0, 179, empty) # 0~179 default 0
+cv2.createTrackbar("Val max", "TrackBar", 255, 255, empty) # 0~255 default 255
+
+while True:
+    if ord("q") == cv2.waitKey(1):
+        break
+
+    cap = cv2.VideoCapture(0)
+    success , img = cap.read()
+    if not success:
+        break
+
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    Hue_min = cv2.getTrackbarPos("Hue min", "TrackBar")
+    Hue_max = cv2.getTrackbarPos("Hue max", "TrackBar")
+    Sat_min = cv2.getTrackbarPos("Sat min", "TrackBar")
+    Sat_max = cv2.getTrackbarPos("Sat max", "TrackBar")
+    Val_min = cv2.getTrackbarPos("Val min", "TrackBar")
+    Val_max = cv2.getTrackbarPos("Val max", "TrackBar")
+
+    # print(Hue_min, Hue_max, Sat_min, Sat_max, Val_min, Val_max)
+
+    lower = np.array([Hue_min, Sat_min, Val_min])
+    upper = np.array([Hue_max, Sat_max, Val_max])
+    #過濾顏色
+    mask = cv2.inRange(hsv, lower, upper)
+    result = cv2.bitwise_and(img, img, mask=mask)
+    cv2.imshow("img", result)
+
+cv2.destroyAllWindows()
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # HSVColorDetection
+HSV和HSL的解釋
+https://zh.wikipedia.org/zh-tw/HSL%E5%92%8CHSV%E8%89%B2%E5%BD%A9%E7%A9%BA%E9%97%B4
